026e426f177ff16d5df34bb9ce12ae38d3551d8d
DPLong <dplong742@gmail.com>
2022-05-31 14:34:14 -0400

    setup for workflow

---
+++ script.js
@@ -0,0 +1,7 @@
+$("#currentDay").text(moment().format("[Today is] dddd, MMM Do, YYYY"));
+
+var rightNow = moment();
+
+//add appropriate class for each children of the container to denote past, present, or future
+
+//localstorage for text area input to save the events. events must persist across reloads
