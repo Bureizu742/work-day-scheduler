6dfb3d8c580afa257f00ec0f5fa416307152c56a
DPLong <dplong742@gmail.com>
2022-05-31 16:37:45 -0400

    time color coding functional

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-$("#currentDay").text(moment().format("[Today is] dddd, MMM Do, YYYY"));
+var now = moment();
+var pastPresentFuture = ["past", "present", "future"];
+var rightNow = now.format("[Today is] dddd, MMM Do, YYYY");
+var hourBlock = [09, 10, 11, 12, 13, 14, 15, 16, 17];
 
-var rightNow = moment();
+$("#currentDay").text(rightNow);
 
-//add appropriate class for each children of the container to denote past, present, or future
-
+//Updates the text area for each block to the appropriate color to denote the passage of time
+function init() {
+    for (var i = 0; i < hourBlock.length; i++) {
+        if (parseInt(now.format("HH")) > hourBlock[i]) {
+            $(".container").children().children().children().eq([i]).children().find("textarea").addClass("past");
+        }
+        if (parseInt(now.format("HH")) === hourBlock[i]) {
+            $(".container").children().children().children().eq([i]).children().find("textarea").addClass("present");
+        }
+        if (parseInt(now.format("HH")) < hourBlock[i]) {
+            $(".container").children().children().children().eq([i]).children().find("textarea").addClass("future");
+        }
+    }
+}
 //localstorage for text area input to save the events. events must persist across reloads
+
+// localStorage.getItem()
+// localStorage.setItem()
+
+console.log(parseInt(now.format("HH")) > 09);
+init();
